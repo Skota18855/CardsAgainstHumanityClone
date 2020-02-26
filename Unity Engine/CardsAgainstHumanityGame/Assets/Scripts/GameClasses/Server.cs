b98983ab1737c6dacff521b51fe11feaa04811e0
Joshua Favre <jfavre390@gmail.com>
2020-02-26 14:57:18 -0700

    Project development and mirror import
    Imported the Mirror networking library and will being implementation

---
+++ Unity Engine/CardsAgainstHumanityGame/Assets/Scripts/GameClasses/Server.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public struct Server
+{
+    string serverName;
+    string serverSecret;
+
+    //endpoint (routing)
+    
+
+    int players;
+    int maxPlayers;
+}
