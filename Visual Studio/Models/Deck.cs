ebd5214496ae1463a27f3c38168fc0bb16e049c3
Lane <lanejm1998@gmail.com>
2020-02-26 15:15:50 -0700

    Finalization of Black Deck. Should function correctly; testing will be done once a basic game is able to be played.

--- Visual Studio/Models/Deck.cs
+++ Visual Studio/Models/Deck.cs
@@ -17,15 +17,15 @@ namespace CardsAgainstHumanityClone.Models
     {
         // Represents all card that are a part of the deck 
         public BlackCard[] DeckCards { get; set; }
+
         // Represents all cards that are a part of the discard pile (if applicable)
         public BlackCard[] DiscardedCards { get; set; }
         
-        // Will return an array of BlackCards equal to the amount specified.
         /// <summary>
         /// Returns an array of BlackCard equal to the amount specified. Cards returned will be removed from the active deck.
         /// </summary>
         /// <param name="amount">The amount of cards to be drawn.</param>
-        /// <returns></returns>
+        /// <returns>An array of BlackCards from the top of the deck (starting at index 0 of the Main Deck array)</returns>
         public BlackCard[] DrawCards(int amount = 1)
         {
             if (amount > DeckCards.Length) throw new ArgumentException("Cannot pass in a value greater than the size of the current deck.");
@@ -43,12 +43,17 @@ namespace CardsAgainstHumanityClone.Models
             {
                 tempDeck[i - amount] = DeckCards[i];
             }
-
+            
             DeckCards = tempDeck;
 
             return result;
         }
 
+        /// <summary>
+        /// Replaces the returnedCards passed in either back into the deck or the discard pile depending on the ReplaceType used. 
+        /// </summary>
+        /// <param name="type">The replace type to use. Discard will push the cards to the discar pile while all other types will place the cards into the main deck as specified.</param>
+        /// <param name="returnedCards"></param>
         public void ReplaceCards(eReplaceType type, BlackCard[] returnedCards)
         {
             int length;
@@ -58,13 +63,14 @@ namespace CardsAgainstHumanityClone.Models
                 case eReplaceType.Discard:
                     length = DiscardedCards.Length + returnedCards.Length;
                     tempDeck = new BlackCard[length];
-                    for (int i = 0; i < DiscardedCards.Length; i++)
+                    int discardLength = DiscardedCards.Length;
+                    for (int i = 0; i < discardLength; i++)
                     {
                         tempDeck[i] = DiscardedCards[i];
                     }
-                    for (int i = DiscardedCards.Length; i < length; i++)
+                    for (int i = discardLength; i < length; i++)
                     {
-                        tempDeck[i] = returnedCards[i - DiscardedCards.Length];
+                        tempDeck[i] = returnedCards[i - discardLength];
                     }
                     DiscardedCards = tempDeck;
                     break;
@@ -72,30 +78,72 @@ namespace CardsAgainstHumanityClone.Models
                     length = DeckCards.Length + returnedCards.Length;
                     tempDeck = new BlackCard[length];
 
-                    foreach(BlackCard card in DeckCards)
+                    int returnedLength = returnedCards.Length;
+                    for (int i = 0; i < returnedLength; i++)
                     {
-                        returnedCards.Append(card);
+                        tempDeck[i] = returnedCards[i];
                     }
+                    for (int i = returnedLength; i < length; i++)
+                    {
+                        tempDeck[i] = DeckCards[i - returnedLength];
+                    }
+
                     DeckCards = tempDeck;
                     break;
                 case eReplaceType.Bottom:
+                    length = DeckCards.Length + returnedCards.Length;
+                    tempDeck = new BlackCard[length];
+
+                    int deckLength = DeckCards.Length;
+                    for (int i = 0; i < deckLength; i++)
+                    {
+                        tempDeck[i] = DeckCards[i];
+                    }
+                    for (int i = deckLength; i < length; i++)
+                    {
+                        tempDeck[i] = returnedLength[i - deckLength];
+                    }
+
+                    DeckCards = tempDeck;
                     break;
                 case eReplaceType.Shuffle:
+                    ReplaceCards(eReplaceType.Top, returnedCards);
+                    ShuffleCards();
                     break;
                 default:
                     break;
             }
         }
 
+        /// <summary>
+        /// Shuffles the cards in the main deck.
+        /// </summary>
+        /// <param name="withDiscard">Defaults to false; determines if the discard pile should be placed back into the deck before shuffling.</param>
         public void ShuffleCards(bool withDiscard = false)
         {
+            if (withDiscard) ReplaceDiscardedCards(eReplaceType.Top);
+            List<BlackDeck> deckList = DeckCards;
+            BlackCard[] deckArray = BlackCard[DeckCards.Length];
+            Random rng = new Random();
 
+            int size = deckArray.Length;
+            for (int i = 0; i < size; i++)
+            {
+                int index = rng.Next(0, deckList.Count());
+                BlackCard card = deckList.ElementAt(index);
+                deckArray[i] = card;
+                deckList.Remove(card);
+            }
         }
 
-        // If shuffle is false, will place 
+        /// <summary>
+        /// Replaces the discard pile back into the main deck in the manner specified by replaceType. The default is to shuffle them back in.
+        /// </summary>
+        /// <param name="replaceType">The type of replacement to do. Selecting Discard will have no effect.</param>
         public void ReplaceDiscardedCards(eReplaceType replaceType = eReplaceType.Shuffle)
         {
-
+            ReplaceCards(replaceType, DiscardedCards);
+            DiscardedCards = new BlackCard[];
         }
     }
     public class WhiteDeck
