94643ac634560387b40d935f98cd628b2573cf63
Joshua Favre <jfavre390@gmail.com>
2020-03-09 18:26:39 -0600

    more javascript stuff
    is starting to work!

--- Visual Studio/wwwroot/js/gameAjax.js
+++ Visual Studio/wwwroot/js/gameAjax.js
@@ -1,5 +1,8 @@
 ﻿var gameBoard = getClasses("boardContainer")[0];
 var playerHand = getClasses("playerHand")[0];
+var playerListElement = getClasses("playerList")[0];
+
+let playerList = [];
 
 var cardDataAttributeString = "data-owner";
 
@@ -23,6 +26,44 @@ function addCard(cardType, cardContent, cardLocation, cardOwner) {
 
 function resetGameBoard(cardContent) {
     gameBoard.innerHTML = `<div class='gameCard blackCard'><p class='blackCardContent'>${cardContent}</p></div >`;
+}
+
+function addPlayer(playerName) {
+
+    let player = {
+        "name": playerName,
+        "score": 0,
+        "isCzar": false,
+    }
+
+    playerList.push(player);
+
+    var elementToAdd = `<div class="player">${playerName} - ${player.score}</div>`
+    playerListElement.innerHTML += elementToAdd;
+}
+
+function setCzar(playerIndex) {
+    if (typeof playerIndex != "number") {
+        return console.error(`setCzar(playerIndex) has been invoked using incorrect arguments. Arguments must be a number less than the current number of players. Argument was: ${playerIndex}; Player count is: ${playerList.length}`);
+    }
+
+    if (playerIndex >= playerList.length || playerIndex < 0) {
+        return console.error(`setCzar(playerIndex) has been invoked using incorrect arguments. Arguments must be a number greater than 0 and less than the current number of players. Argument was: ${playerIndex}; Player count is: ${playerList.length}`);
+    }
+
+    playerList.forEach(p => p.isCzar = false);
+    
+    playerList[playerIndex].isCzar = true;
+
+    let playerElements = getClasses("player");
+
+    for (let i = 0; i < playerElements.length; i++) {
+        if (playerElements[i].innerHTML.endsWith(" - CZAR")) {
+            playerElements[i].innerHTML = playerElements[i].innerHTML.substring(0, playerElements[i].innerHTML.length - 6);
+        }
+    }
+    
+    playerElements[playerIndex].innerHTML += " - CZAR";
 }
 
 function addCardTo(htmlElement, cardType, cardContent, dataAttributeValue) {
